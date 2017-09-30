ab531095d78debcbe06327591054942915c44c63
nigue <anibaldavor@gmail.com>
2017-09-30 17:31:50 -0300

    avance

---
+++ 05-03-ControllersHandling/android/src/main/java/com/gdx/learn/android/PadHandlingTestActivity.java
@@ -0,0 +1,18 @@
+package com.gdx.learn.android;
+
+import com.gdx.learn.core.PadHandlingTest;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+
+public class PadHandlingTestActivity extends AndroidApplication {
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+			super.onCreate(savedInstanceState);
+			AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+			initialize(new PadHandlingTest(), config);
+	}
+}
